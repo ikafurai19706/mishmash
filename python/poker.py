c7d4b539c970da62a806b9c3edf8313d3156377c
it0maki_ey <ikafurai19706@gmail.com>
2024-06-07 16:57:13 +0900

    update

--- python/poker.py
+++ python/poker.py
@@ -1,2 +1,26 @@
-import DictDotNotation as DDN
+#coding: utf-8
 
+symbols = {"spade": "♠", "heart": "\033[31m♥\033[0m", "d": "\033[31m♦\033[0m", "club": "♣"}
+numbers = [" A", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10", " J", " Q", " K"]
+
+def CardPrint(num, sym):
+    print("""\
+┌───┐\033[1B\033[5D\
+│{} │\033[1B\033[5D\
+│ {} │\033[1B\033[5D\
+└───┘\033[3A"""
+          .format(num, sym), end="")
+
+title_logo = """\
+┌─────┐┌─────┐┌─┐ ┌─┐
+│ ┌─┐ ││ ┌─┐ ││ │┌┘ │
+│ └─┘ ││ │ │ ││ └┘
+│ ┌───┘│ │ │ ││ 
+│ │    │ └─┘ ││ ┌┐ 
+└─┘    └─────┘└─┘
+"""
+print("\033[2J\033[0;0H"+title_logo)
+for i in symbols.values():
+    for j in numbers:
+        CardPrint(j, i)
+    print("\033[4E", end="")
